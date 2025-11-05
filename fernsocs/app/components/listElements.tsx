8f095234b593f78b5c276cb8773ba57755d98cba
StargazerCatsriel <danobrovold@gmail.com>
2025-11-05 09:40:29 +0100

    Fix character List

--- fernsocs/app/components/listElements.tsx
+++ fernsocs/app/components/listElements.tsx
@@ -60,17 +60,21 @@ interface setListProps{
 export default function SetList({charactersData}:setListProps){
 
     function handleClick(){
-
+        
     }   
 
     return(    
             <div className="flex flex-col gap-6 w-full justify-center items-center">
                 {charactersData.map((character: Character, index:number) => (
-                    <div key={index} className="flex gap-20 items-center mt-10 bg-black/75 w-[50%] p-5 rounded-2xl">
-                        <div className="flex w-50 h-50">
-                            <Image className="object-cover w-50 h-50" src={character.descriptions.imageURL} alt={character.descriptions.imageAlt} width={1000} height={1000} quality={75}></Image>
+                    <div key={index} className="flex gap-20 items-center mt-10 bg-black/75 w-[90%] sm:w-[50%] p-5 rounded-2xl transition duration-300 ease-in-out hover:translate-y-1 hover:scale-110 ">
+                        <div className="flex">
+                            <Image className="object-cover w-[150px] h-[150px] sm:w-[300] sm:h-[300px] rounded-xl" src={character.descriptions.imageURL} alt={character.descriptions.imageAlt} width={1000} height={1000} quality={75}></Image>
                         </div>
-                        <h1 className="text-2xl font-bold">{character.characterProfile.fullName}</h1>
+
+                        <div className="text-white dark:text-black">
+                            <h1 className="text-3xl font-bold">{character.characterProfile.fullName}</h1>
+                            <h2 className="text-xl">{character.universeInfo.mainUniverse}</h2>
+                        </div>
                     </div>
                 ))}
             </div>
--- fernsocs/app/components/searchmode.tsx
+++ fernsocs/app/components/searchmode.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const buttonClass = "p-2 rounded-sm hover:bg-gray-100 dark: hover:bg-zinc-900";
+const buttonClass = "p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-zinc-900";
 
 export default function SearchOptions(){
     return(
