556e6c367ba3012b454dba531d967c97875ba4de
StargazerCatsriel <danobrovold@gmail.com>
2025-11-07 13:43:47 +0100

    Add Window Popup that uses the right character's Index

--- fernsocs/app/components/window.tsx
+++ fernsocs/app/components/window.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import * as CharacterTypes from "../types/characterTypes"
 
-export default function PopupWindow({charactersData}:CharacterTypes.setListProps){
+interface setProps{
+    charactersData:CharacterTypes.Character[];
+    currentIndex:number;
+}
+
+export default function PopupWindow({charactersData, currentIndex}:setProps){
     return(
         <div className="w-[95%] sm:w-[80%] rounded-xl text-black p-5 fixed top-1/2 -translate-y-1/2 shadow-md/5 z-100 bg-white left-1/2 -translate-x-1/2 transform h-[90%]">
-            <h1>{charactersData[0].characterProfile.fullName}</h1>
+            <h1>{charactersData[currentIndex].characterProfile.fullName}</h1>
         </div>
     )
 }
