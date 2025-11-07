b3735c1768cc3957a4e1bafccd1c0e53029becf1
StargazerCatsriel <danobrovold@gmail.com>
2025-11-07 13:23:47 +0100

    Start Adding a window

--- fernsocs/app/page.tsx
+++ fernsocs/app/page.tsx
@@ -1,16 +1,14 @@
-import Image from "next/image";
-import TestFetch from "./components/fetchComponent";
 import Link from "next/link";
 import SearchOptions from "./components/searchmode";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <h1 className="text-4xl p-5">Fern's OC Storage</h1>
-      <input type="search" id="search" name="searchField" size={20} className="border p-5 text-xl rounded-4xl text-center sm:w-[50%]" placeholder="Enter Search"></input>
+    <div className="flex justify-center items-center h-screen flex-col bg-neutral-200">
+      <h1 className="text-4xl p-5 font-bold">Fern's OC Storage</h1>
+      <input type="search" id="search" name="searchField" size={20} className="border p-5 text-xl rounded-4xl text-center sm:w-[50%] bg-white" placeholder="Enter Search"></input>
       <SearchOptions></SearchOptions>
 
-      <Link href={"/list"} className="text-xl fixed bottom-5 right-5">Go to Character List instead</Link>
+      <Link href={"/list"} className="text-xl fixed bottom-5 right-5 bg-white p-2 rounded-2xl dark:text-black">Go to Character List instead</Link>
     </div>
   );
 }
